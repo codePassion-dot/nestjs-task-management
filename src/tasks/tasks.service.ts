d8fed91f54c2ea66b61986dba222e0cc44516905
Juan Jacobo Jaramillo Aristizabal <jjjaramilloar@unal.edu.co>
2022-01-04 17:11:17 -0500

    Added validation for nonexistent task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -43,7 +43,11 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+    if (!found) {
+      throw new NotFoundException();
+    }
+    return found;
   }
 
   deleteTaskById(targetId: string): string {
