ac731987964b213b61246a80983ad244ae2682a6
Juan Jacobo Jaramillo Aristizabal <jjjaramilloar@unal.edu.co>
2022-01-04 16:16:59 -0500

    Added get task with filters operation

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,9 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
+
+  getTaskWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter((task) => {
+        if (task.title.includes(search) || task.description.includes(search)) {
+          return true;
+        }
+        return false;
+      });
+    }
+
+    return tasks;
+  }
 
   getAllTasks() {
     return this.tasks;
