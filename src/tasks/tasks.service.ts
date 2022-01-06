728511d6bb41f783a4b2936a36be2ee96c12266b
Juan Jacobo Jaramillo Aristizabal <jjjaramilloar@unal.edu.co>
2022-01-05 19:28:00 -0500

    Cleaned up mess in task service, the service is in charge of validation and core logic but no the one involved directly with the database

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -29,16 +29,8 @@ export class TasksService {
   // getAllTasks() {
   //   return this.tasks;
   // }
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-
-    const task = this.tasksRepository.create({
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    });
-    await this.tasksRepository.save(task);
-    return task;
+  createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.tasksRepository.createTask(createTaskDto);
   }
 
   async getTaskById(id: string): Promise<Task> {
